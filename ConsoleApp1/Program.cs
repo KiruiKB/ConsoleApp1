335300b686a4ce617ecf1548aa06ead35d3edee0
Kirui Boniface <kiruibonie@gmail.com>
2022-05-19 22:53:26 +0300

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = 1;
+            for (int i = 0; i < 4; i++)
+            {
+                for (int j = 0; j < num; j++)
+                {
+                    Console.Write("*");
+                }
+                num++;
+                Console.WriteLine();
+            }
+            Console.ReadLine();
+
+        }
+    }
+}
